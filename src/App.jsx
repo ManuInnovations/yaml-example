27b7caeacefc057b10ad336ab6938f847ec62a28
Janie Tito <jtito@hotmail.com>
2018-02-28 15:06:58 +1300

    feat($analytics): Embedded the Countly script into the top level App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Router } from 'react-static';
 import Routes from 'react-static-routes'; // eslint-disable-line
+import $ from 'jquery';
+import Countly from 'countly-sdk-web';
 import styled from 'react-emotion';
 import Nav from './components/Nav';
 import './styles/main.css';
@@ -16,11 +18,53 @@ const Container = styled.div`
   padding: 0;
 `;
 
-export default () => (
-  <Router>
-    <Container>
-      <Nav />
-      <Routes />
-    </Container>
-  </Router>
-);
+class App extends React.Component {
+  componentWillMount() {
+    console.log('Countly script is here!')
+
+    var embedCode = `
+    <script type='text/javascript'>
+      {/* some default pre init */}
+      var Countly = Countly || {};
+      Countly.q = Countly.q || [];
+      
+      {/* provide your app key that you retrieved from Countly dashboard */}
+      Countly.app_key = "YOUR_APP_KEY";
+      
+      {/* provide your server IP or name. Use try.count.ly for EE trial server. */}
+      {/* if you use your own server, make sure you have https enabled if you use
+      https below. */}
+      Countly.url = "https://yourdomain.com"; 
+      
+      {/* start pushing function calls to queue
+      track sessions automatically */}
+      Countly.q.push(['track_sessions']);
+        
+      {/* track sessions automatically */}
+      Countly.q.push(['track_pageview']);
+        
+      {/* load countly script asynchronously */}
+      (function() {
+        var cly = document.createElement('script'); cly.type = 'text/javascript'; 
+        cly.async = true;
+        // enter url of script here (see below for other option)
+        cly.src = 'https://cdn.jsdelivr.net/npm/countly-sdk-web@latest/lib/countly.min.js';
+        cly.onload = function(){Countly.init()};
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(cly, s);
+      })();
+    </script>`
+    $('.main').append(embedCode);
+  }
+  render() {
+    return (
+      <Router>
+        <Container className='main'>
+          <Nav />
+          <Routes />
+        </Container>
+      </Router>
+    )
+  }
+}
+
+export default App;
