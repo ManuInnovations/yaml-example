bed148b77fac12576e0c98959ac8f6766dd3023e
Janie Tito <jtito@hotmail.com>
2018-03-01 16:45:04 +1300

    fix($404): Added a sorry image to the 404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import content from '../content';
+import { Images } from '../assets';
+
 
 export default () => (
-  <div style={{ minHeight: 'calc(100vh - 220px)' }}>
+  <div style={{ minHeight: 'calc(100vh - 200px)' }}>
     <h1>{content('pages.error.text')}</h1>
+    <img src={Images.sorry} />
   </div>
 );
